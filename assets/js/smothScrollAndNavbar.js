2f7c01d0eed09a8cc143e503be19bc8dbbef2eea
sptrcode <ilhamsyn7@gmail.com>
2017-10-04 08:58:08 +0700

    Initial Commit

---
+++ assets/js/smothScrollAndNavbar.js
@@ -0,0 +1,28 @@
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+
+// navigation scroll
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    // var offset = 69;
+    var offset = 0;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 800);
+    event.preventDefault();
+});
